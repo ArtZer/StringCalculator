d2a0b16d7c9f58b756e20ea88bdebd1a36a698a5
Akhatov Artur <artzer592@gmail.com>
2021-03-28 12:21:46 +0500

    write calculate but have error

--- Calculate.cs
+++ Calculate.cs
@@ -1,15 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace StringCalculator
 {
     class Calculate
     {
-        public int PolishNotation(Queue<string> exeption)
+        public double PolishNotation(Queue<string> exeption)
         {
+            double rezult;
+            Stack<double> operand = new Stack<double>();
 
-            return 0;
+            rezult = int.Parse(exeption.Dequeue());
+            while (exeption.Count > 0)
+            {
+                string temp = exeption.Dequeue();
+                if (CheckOperand(temp))
+                    operand.Push(int.Parse(temp));
+                else
+                {
+                    double op1 = operand.Pop();
+                    operand.Push(CalculateTwoOpreands(op1, operand.Pop(), temp));
+                }  
+            }
+
+            return operand.Pop();
+        }
+
+        private bool CheckOperand(string st)
+        {
+            Regex regex = new Regex(@"\d");
+            MatchCollection matches = regex.Matches(st);
+            return (matches.Count > 0);
+        }
+
+        private double CalculateTwoOpreands(double op1, double op2, string operatoR)
+        {
+            switch (operatoR)
+            {
+                case "+": return op1 + op2;
+                case "-": return op1 - op2;
+                case "/": return op1 / op2;
+                case "*": return op1 * op2;
+                case "^": return Math.Pow(op1,op2);
+                default: return 0;
+            }
         }
     }
 }
--- Calculator.cs
+++ Calculator.cs
@@ -8,29 +8,31 @@ namespace StringCalculator
     {
         private string expression;
         private Queue<string> transformExeption = new Queue<string>();
-        public string Solution(string originExeption)
+        public double Solution(string originExeption)
         {
             Transform transform = new Transform();
             Validation validation = new Validation();
+            Calculate calculate = new Calculate();
+
             try
             {
                 string temp = validation.Check(originExeption);
                 if (temp != "")
                 {
                     Console.WriteLine(temp);
-                    return "";
+                    return 0;
                 }
 
                 transformExeption = transform.PolishNotation(originExeption);
+
+                return calculate.PolishNotation(transformExeption);
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Exeption: " + ex);
             }
-                      
 
-
-            return "";
+            return 0;
         }
     }
 }
